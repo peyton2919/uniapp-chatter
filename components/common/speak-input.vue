441a050c0db8cfadebab7faa417febd79418a5ef
peyton2919 <fz2919@tom.com>
2023-09-26 23:18:09 +0800

    update 20230926 23:17

---
+++ components/common/speak-input.vue
@@ -0,0 +1,43 @@
+<template>
+	<!-- 底部发言栏 -->
+	<view class="fixed-bottom h-100 flex align-center border-top bg-white">
+		<input type="text" class="flex-1 rounded bg-light ml-2"
+				style="padding: 5rpx; z-index: 99;" placeholder="文明发言"
+				v-model="content" :adjust-position="false"
+				@confirm="submit"/>
+				
+		<view class="iconfont icon-fabu i-100 flex align-center justify-center font-lg animated"
+				hover-class="jello text-main"
+				@click="submit"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				content:""
+			}
+		},
+		methods:{
+			// 发送
+			submit(){
+				
+				if(this.content === ''){
+					return uni.showToast({
+						title: '消息不能为空!',
+						icon:'none'
+					});
+				}
+				this.$emit('submit',this.content)
+				
+				// 清空输入框
+				this.content=''
+				
+			},
+		}
+	}
+</script>
+
+<style>
+</style>
